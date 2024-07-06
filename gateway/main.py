e96ff14629044ac8d60628949b30593519410bb0
DongHunKim <ddong3525@gmail.com>
2024-07-06 22:23:27 +0900

    Refactor api code to enhance readability by employing class and functions and logger function

--- gateway/main.py
+++ gateway/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, HTTPException, File, UploadFile, APIRouter
 from dotenv import load_dotenv
 import base64
 import pika
@@ -9,84 +8,102 @@ import timeit
 import hashlib
 import redis
 import json
+import logging
 from datetime import datetime
 
 
-def generate_hash_key(file_content):
-    return hashlib.sha256(file_content.encode()).hexdigest()
+class Blast:
+    def __init__(self):
+        self.client = self._init_redis()
+        self.channel = self._init_rabbitmq()
+        self.cache_storage = "cache_storage"
+        self.router = APIRouter()
+        self.router.add_api_route("/blast", self.blast, methods=["GET"])
 
 
-app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+    def _init_redis(self):
+        REDIS_URL = os.environ.get("REDIS_URL")
+        REDIS_PORT = os.environ.get("REDIS_PORT")
+        client = redis.StrictRedis(host=REDIS_URL, port=REDIS_PORT, db=0, decode_responses=True, socket_timeout=10)
+        client.flushdb()
+
+        return client
+
+    def _init_rabbitmq(self):
+        # Connect to RabbitMQ
+        RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_URL))
+        channel = connection.channel()
+        channel.queue_declare(queue="gateway_service")
+        channel.queue_declare(queue="blast_service")
+
+        return channel
+
+    def check_cache(self, hash_key):
+        if self.client.exists(hash_key):
+            logger.info(f"Hash key {hash_key} hit in Redis!")
+            cached_data = self.client.get(hash_key)
+            if os.path.exists(cached_data):
+                with open(cached_data, "r") as f:
+                    data = json.load(f)
+
+                return data
+            else:
+                logger.warning("But cache file does not exist!")
+        else:
+            logger.info(f"Hash key doesn't exist")
+        return ""
+
+
+    def cache_data(self, hash_key, data, file_name):
+        timestamp = datetime.now().strftime("%y%m%d%H%M")
+        cache_file_name = f"{timestamp}_{"".join(file_name.split(".")[:-1])}.json"
+
+        cache_file_dir = os.path.join(self.cache_storage, cache_file_name)
+
+        with open(cache_file_dir, "w") as wf:
+            wf.write(json.dumps(data))
+
+        logger.info(f"cache data to {cache_file_dir}")
+
+        self.client.set(hash_key, cache_file_dir)
+
+
+    def blast(self, user_id: str, outfmt: int = 5, file: UploadFile = File(...)):
+        start = timeit.default_timer()
+        file_name = file.filename
+        logger.info(f"Received user_id: {user_id}, outfmt: {outfmt}, file_name: {file_name}")
+
+        with open(file_name, "wb") as buffer:
+            buffer.write(file.file.read())
+
+        blast_rpc = rpc_client.BlastRpcClient()
+
+        with open(file_name, "rb") as buffer:
+            file_data = buffer.read()
+            file_base64 = base64.b64encode(file_data).decode()
+
+        hash_key = hashlib.sha256(file_base64.encode()).hexdigest()
+
+        if not (response:=self.check_cache(hash_key)):
+            request_json = {"user_id": user_id, "outfmt": outfmt, "file": file_base64}
+
+            response = blast_rpc.call(request_json)
+
+            self.cache_data(hash_key, response, file_name)
+
+            os.remove(file.filename)
+
+        logger.info(f"Done! took {timeit.default_timer() - start}s.")
+          
+        return response
 
 # Load environment variables
 load_dotenv()
-RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
-
-# Connect to RabbitMQ
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(RABBITMQ_URL)
-)  # add container name in docker
-channel = connection.channel()
-channel.queue_declare(queue="gateway_service")
-channel.queue_declare(queue="blast_service")
-
-# Connect to Redis
-client = redis.StrictRedis(
-    host="localhost", port=6379, db=0, decode_responses=True, socket_timeout=10
-)
-client.flushdb()
-
-
-@app.get("/", tags=["Welcome"])
-def hello_world():
-    return {"message": "hello world"}
-
-
-@app.get("/blast", tags=["Blast Service"])
-def blast(user_id: str, outfmt: int = 5, file: UploadFile = File(...)):
-    start = timeit.default_timer()
-    print(f"Received user_id: {user_id}, outfmt: {outfmt}, file: {file.filename}")
-
-    with open(file.filename, "wb") as buffer:
-        buffer.write(file.file.read())
-
-    blast_rpc = rpc_client.BlastRpcClient()
-
-    with open(file.filename, "rb") as buffer:
-        file_data = buffer.read()
-        file_base64 = base64.b64encode(file_data).decode()
-
-    hash_key = generate_hash_key(file_base64)
-
-    if client.exists(hash_key):
-        print(f"Hash key {hash_key} hit in Redis!")
-        cached_data = client.get(hash_key)
-        if os.path.exists(cached_data):
-            with open(cached_data, "r") as f:
-                data = json.load(f)
-            return data
-        else:
-            print("But cache file does not exist!")
-
-    request_json = {"user_id": user_id, "outfmt": outfmt, "file": file_base64}
-
-    response = blast_rpc.call(request_json)
-
-    # Format the date and time as YYMMDDHHMM
-    timestamp = datetime.now().strftime("%y%m%d%H%M")
-    cache_storage = "cache_storage"
-    file_name = "".join(file.filename.split(".")[:-1])
-    cache_file_name = f"{timestamp}_{file_name}.json"
-    cache_file_dir = os.path.join(cache_storage, cache_file_name)
-    with open(cache_file_dir, "w") as wf:
-        wf.write(json.dumps(response))
-
-    client.set(hash_key, cache_file_dir)
-
-    os.remove(file.filename)
-    print(f"Done! took {timeit.default_timer() - start}s.")
-    return response
+app = FastAPI()
+blast_router = Blast().router
+app.include_router(blast_router)
+logger = logging.getLogger("uvicorn")
 
 
 if __name__ == "__main__":
--- gateway/rpc_client.py
+++ gateway/rpc_client.py
@@ -1,47 +1,53 @@
 import pika
 import uuid
 import json
+import logging
 from dotenv import load_dotenv
 import os
 
 # Load environment variables
 load_dotenv()
 RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
+logger = logging.getLogger("uvicorn")
 
 
 class BlastRpcClient(object):
     def __init__(self):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=RABBITMQ_URL))
+            pika.ConnectionParameters(host=RABBITMQ_URL)
+        )
 
         self.channel = self.connection.channel()
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
+        result = self.channel.queue_declare(queue="", exclusive=True)
         self.callback_queue = result.method.queue
 
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
-            auto_ack=True)
+            auto_ack=True,
+        )
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
 
     def call(self, message: dict):
-        print("Pushing to Rabbitmq...")
+        logger.info("Pushing to Rabbitmq...")
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
-            exchange='',
-            routing_key='blast_service',
+            exchange="",
+            routing_key="blast_service",
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
             ),
-            body=json.dumps(message))
+            body=json.dumps(message),
+        )
         while self.response is None:
             self.connection.process_data_events()
+        logger.info("Message received from Rabbitmq...")
         response_json = json.loads(self.response)
 
         return response_json
